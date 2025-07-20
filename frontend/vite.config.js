712436694a55dec8fef7b97df22848a058f59caa
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-07-20 13:49:49 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/, // include only your .js files in src that use JSX
+  },
 })

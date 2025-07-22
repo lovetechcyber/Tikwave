3190edcc01cd2af9f3e66b0c696531527b10cf98
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-07-22 20:24:20 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,16 @@
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 // Pages
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 
 
-// Context Providers
-import { AuthProvider } from "./context/AuthContext";
-import { LanguageProvider } from "./context/LanguageContext";
-
-// Auth Guard
-import ProtectedRoute from "./components/ProtectedRoute";
-
 function App() {
   return (
-    <LanguageProvider>
-      <AuthProvider>
-        <Router>
-          <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<AuthPage />} />
             <Route path="/signup" element={<AuthPage />} /> 
-          </Routes>
-      </AuthProvider>
-    </LanguageProvider>
   );
 }
 

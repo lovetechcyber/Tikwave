9dd832d47d08f0392d175852036d165d72ae0860
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-07-22 20:44:15 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
-
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 // Pages
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 
-
 function App() {
   return (
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/signup" element={<AuthPage />} /> 
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<AuthPage />} />
+      <Route path="/signup" element={<AuthPage />} />
+    </Routes>
   );
 }
 
